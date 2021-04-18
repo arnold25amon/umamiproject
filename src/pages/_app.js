24da7a76cc242cb4dc1ef0ebf18bf00b9b048600
Arnold Amon <arnoldleonardamon@gmail.com>
2021-04-18 15:39:00 +0530

    new add

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,4 @@
 import Navbar from '../components/navbar'
-import { ChakraProvider } from '@chakra-ui/react';
-import Layout from '../components/Layout'
 import '../../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
