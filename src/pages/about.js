0302763d225b034f0364ab54261f1c5f84b9ed0e
Arnold Amon <arnoldleonardamon@gmail.com>
2021-04-24 13:39:25 +0530

    ok both are hybrid

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../../styles/Layout.module.css";
 import { Typography, Card, Container, StylesProvider } from "@material-ui/core";
-// export const config = { amp: true };
+export const config = { amp: "hybrid" };
 const Recipe = ({ res }) => {
   return (
     <Container style={{ padding: "4px" }}>
